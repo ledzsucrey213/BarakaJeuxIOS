241f7b289f36b1476ccc02a51429bcedcadcd348
etud <yanis.baroudi@etu.umontpellier.fr>
2025-03-14 17:59:29 +0100

    first commit

---
+++ BarakaJeux/BarakaJeuxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarakaJeuxApp.swift
+//  BarakaJeux
+//
+//  Created by etud on 14/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct BarakaJeuxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
